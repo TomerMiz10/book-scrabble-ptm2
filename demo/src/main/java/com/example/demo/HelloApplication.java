c0a6ac8d1baf25df4a4e206bcb04916dd1b2d4c1
Omer Maoz <omermaoz1998@gmail.com>
2023-04-30 14:51:02 +0300

    added app icon

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,16 +13,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view.fxml"));
-
-        FXMLLoader fxmlLoaderP = new FXMLLoader(HelloApplication.class.getResource("Play-Local.fxml"));
-        FXMLLoader fxmlLoaderN = new FXMLLoader(HelloApplication.class.getResource("Network&communication.fxml"));
-        FXMLLoader fxmlLoaderG = new FXMLLoader(HelloApplication.class.getResource("Guest-Mode.fxml"));
-
         Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
-        stage.setTitle("Hello!");
         stage.setScene(scene);
+        Image anotherIcon = new Image("file:src files/bookScrabble.jpg");
+        stage.getIcons().add(anotherIcon);
+        stage.setTitle("BookScrabble Game");
         stage.show();
+
     }
 
     public static void main(String[] args) {
