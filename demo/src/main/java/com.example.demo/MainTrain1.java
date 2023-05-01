8423b71605a68aa0a742ee5524c4948baccf2d79
omerone <omermaoz1998@cs.colman.ac.il>
2023-05-01 18:31:47 +0300

    adding mainTrain testing class

--- demo/src/main/java/com.example.demo/MainTrain1.java
+++ demo/src/main/java/com.example.demo/MainTrain1.java
@@ -1,12 +1,22 @@
 package com.example.demo;
 
 public class MainTrain1 {
-    public static void main(String[] args) {
-           serverAndClientTest();
-    }
     //server test
     public static void serverAndClientTest(){
         Server server = new Server();
+        Thread thread = new Thread(server);
+        thread.start();
         server.connectClient();
+    }
+    //buttons test
+    public static void buttonsTest(){
+
+    }
+
+
+    //Main
+    public static void main(String[] args) {
+           serverAndClientTest();
+           buttonsTest();
     }
 }
